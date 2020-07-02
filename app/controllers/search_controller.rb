b18a7c6e8f505c64adb845237d6d3ff8ec220873
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-02 10:57:02 -0600

    Add member class and test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,19 @@
 class SearchController < ApplicationController
   def index
-    state = params[:state]
 
-    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-KEY"] = ''
-    end
 
-    response = conn.get("/congress/v1/members/house/#{state}/current.json")
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    @members = json[:results]
+    # state = params[:state]
+
+    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+    #   faraday.headers["X-API-KEY"] = ENV['propublica_key']
+    # end
+
+    # response = conn.get("/congress/v1/members/house/#{state}/current.json")
+
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @members = json[:results].map do |member_data|
+    #   Member.new(member_data)
+    # end
   end
 end
